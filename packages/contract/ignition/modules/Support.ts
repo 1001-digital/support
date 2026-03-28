0cc7c870cc08c9e71431fcedff4424cc04862234
yougogirldoteth <linus@1001.digital>
2026-03-28 23:33:16 +0100

    Update ignition module for new constructor params

--- packages/contract/ignition/modules/Support.ts
+++ packages/contract/ignition/modules/Support.ts
@@ -1,12 +1,18 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("SupportModule", (m) => {
+  const projectName = m.getParameter("projectName");
+  const projectSymbol = m.getParameter("projectSymbol");
+  const logo = m.getParameter("logo");
   const priceFeed = m.getParameter("priceFeed");
   const tierPrices = m.getParameter("tierPrices");
   const discountMinMonths = m.getParameter("discountMinMonths");
   const discountPercentOff = m.getParameter("discountPercentOff");
 
   const support = m.contract("Support", [
+    projectName,
+    projectSymbol,
+    logo,
     priceFeed,
     tierPrices,
     discountMinMonths,
