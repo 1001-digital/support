e924cd46ea6d19485978086be294e48b2bf2cb6d
jalil <jalil@1001.digital>
2026-04-02 00:34:34 +0200

    Modules and wire helpers for hooks

---
+++ packages/contract/ignition/modules/DiscountHook.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("DiscountHookModule", (m) => {
+  const minMonths = m.getParameter("minMonths");
+  const percentOff = m.getParameter("percentOff");
+
+  const discountHook = m.contract("DiscountHook", [minMonths, percentOff]);
+
+  return { discountHook };
+});
